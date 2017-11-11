6d5efe10de2209fbc6556980f46745a7fab7649e
Diego Giraldo <dgiral58@eafit.edu.co>
2017-11-10 21:35:26 -0500

    Autolayout

---
+++ 3-Autolayout/iOSAutolayoutDiegoGiraldo/iOSAutolayoutDiegoGiraldo/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  iOSAutolayoutDiegoGiraldo
+//
+//  Created by Diego Giraldo on 6/07/16.
+//  Copyright © 2016 NextU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
